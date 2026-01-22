35601bbb6ecb5e4a8e248db80e5b0510a3e377bc
mypengpengli <306100898@qq.com>
2026-01-22 11:51:32 +0800

    改造为 Vite + React + TypeScript 项目

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World!</h1>
+      <p>欢迎来到 AI随学 - aisuixue.com</p>
+      <p>网站部署成功！</p>
+      <p className="tech-stack">Vite + React + TypeScript</p>
+    </div>
+  )
+}
+
+export default App
