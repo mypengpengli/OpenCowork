9dba6f2746ed31bedd18be861fe3bb392a763118
mypengpengli <306100898@qq.com>
2026-01-22 12:54:44 +0800

    添加联系邮箱

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
       <p>欢迎来到 AI随学 - aisuixue.com</p>
       <p>网站部署成功！</p>
       <p className="tech-stack">Vite + React + TypeScript</p>
+      <p className="contact">联系邮箱：306100898@qq.com</p>
     </div>
   )
 }
